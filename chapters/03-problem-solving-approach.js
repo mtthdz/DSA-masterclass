20fb57f32d61388550c75902778fbc079aaa48b1
mtthdz <matt@mtthdz.com>
2022-11-10 07:48:59 -0500

    completed chapter 3 - problem solving approach

---
+++ chapters/03-problem-solving-approach.js
@@ -0,0 +1,24 @@
+/**
+ * Chapter 03 - Algorithms and Problem Solving Approach
+ * 
+ * An algorithm is a process or set of steps to accomplish
+ * a certain task.
+ * It's important to understand the problem. A few points
+ * to keep in mind when solving a problem:
+ *  1. can I restate the problem?
+ *  2. what are the inputs?
+ *  3. what are the outputs?
+ *  4. can the outputs be determined from inputs?
+ *  5. How should I label the important data?
+ * 
+ * When solving, it's a good idea to start with simple examples
+ * before exploring more complex edgecases
+ * It's also good to explore valid versus invalid inputs
+ * 
+ * When refactoring, there's a few questions that we can ask:
+ *  1. can you derive the result differently?
+ *  2. can you use the result or method for some other problem?
+ *  3. can you improve the performance of your solution?
+ *  4. how have other people solved this problem?
+ *  
+ */
