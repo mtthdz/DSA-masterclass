474f133b7300ca69bf9016eb550d61819abaab96
mtthdz <matt@mtthdz.com>
2023-03-09 08:11:30 -0500

    4-2 multiple pointers example problem

--- chapters/04-problem-solving-patterns/4-2-multiple-pointers.js
+++ chapters/04-problem-solving-patterns/4-2-multiple-pointers.js
@@ -5,4 +5,51 @@
  * position and move towards the beginning, end, or middle based
  * on a certain condition
  * 
+ * Example 4.2A
+ * Write a function called SumZero which accepts a sorted array
+ * of integers. The function should find the first pair where the 
+ * sum is 0. Return an array that includes both values that sum to
+ * zero or undefined if a pair does not exist.
+ * Note: this only works for sorted arrays
+ * 
+ * sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
+ * sumZero([-2,0,1,3])         // undefined
+ * sumZero([1,2,3])            // undefined
  */
+
+// Naive Solution, O(n^2)
+function sumZeroNaive(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+/**
+ * Multiple Pointers, O(n)
+ * 
+ * Note: we don't need to explicity state if (sum < 0),
+ * it's present for quick reference.
+ *
+ * @param {*} arr 
+ * @returns 
+ */
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else if (sum < 0) {
+            left++;
+        }
+    }
+}
