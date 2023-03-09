54f59b2b043b182d4fd36a7bf90465dd0b5d57b3
mtthdz <matt@mtthdz.com>
2023-03-09 11:35:37 -0500

    completed multiple pointers question

--- chapters/04-problem-solving-patterns/4-2-multiple-pointers.js
+++ chapters/04-problem-solving-patterns/4-2-multiple-pointers.js
@@ -53,3 +53,53 @@ function sumZero(arr) {
         }
     }
 }
+
+/**
+ * Example 4.2B
+ * Implement a function called countUniqueVlaues,
+ * which accepts a sorted array, and counts the unique
+ * values in the array. There can be negative numbers, 
+ * but it'll always be sorted.
+ * 
+ * countUniqueValues([1,1,1,1,1,2])              // 2
+ * countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
+ * countUniqueValues([])                         // 0
+ * countUniqueValues([-2,-1,-1,0,1])             // 4
+ * 
+ * @param {number}   arr
+ * @returns {number} count
+ */
+function countUniqueValues(arr) {
+  /**
+   * we check to see if pointer A matches B;
+   * if they don't, we move pointer A to the right,
+   * and assign pointer A to pointer B's value.
+   *
+   * Once pointer B reaches the end of the array,
+   * pointer A will be set at the last unique number.
+   * Coincidentally, pointer A's value denotes the number
+   * of unique values (see example below)
+   * 
+   * Starting position:
+   *  i
+   * [1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 6, 6, 6, 7]
+   *     j
+   * 
+   * 
+   * By the end of the loop:
+   *                    i
+   * [1, 2, 3, 4, 5, 6, 7, 6, 6, 7]
+   *                             j
+   */
+  let left = 0
+
+  if (arr.length === 0) return 0;
+  for (let right = 1; right < arr.length; right++) {
+    if (arr[left] !== arr[right]) {
+      left ++;
+      arr[left] = arr[right];
+    }
+  }
+
+  return left + 1;
+}
