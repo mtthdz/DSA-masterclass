da7746a4e85cf89a22ecc0b30f8266c7534a1052
mtthdz <matt@mtthdz.com>
2024-07-16 11:53:15 -0400

    started singly linked lists

--- chapters/7-sorting-algorithms/7-4-merge-sort.js
+++ chapters/7-sorting-algorithms/7-4-merge-sort.js
@@ -16,3 +16,31 @@
  * [3, 4, 5, 8]   [1, 2, 6, 7]  -> sort, merge
  * [1, 2, 3, 4, 5, 6, 7, 8]     -> sort, merge
  */
+
+/**
+ * Merging Arrays
+ * In order to implement merge sort, it's useful to first
+ * implement a function responsible for merging two sorted
+ * arrays.
+ * Given two sorted arrays, this helper should create a new
+ * array (which is also sorted) by combining both arrays
+ * Should be O(n + m) time and O(n + m) space complexity
+ * 
+ * Pseudocode
+ * -> create an empty array, take a look at the smallest values
+ *    in each input array
+ * -> while there are still values we haven't look at
+ *    -> if first array[n] < second array[n] push first array[n]
+ *    -> if first array[n] > second array[n] push second array[n]
+ *    -> once array is done, push remaining values in other array
+ */
+
+
+/**
+ * Visual explanation
+ * [1, 10, 50] [2, 14, 99, 100] -> 1, 2   => [1]
+ * [10, 50] [2, 14, 99, 100]    -> 10, 2  => [1, 2]
+ * [10, 50] [14, 99, 100]       -> 10, 14 => [1, 2, 10]
+ * etc.
+ */
+merge([1, 10, 50], [2, 14, 99, 100]);
---
+++ chapters/8-data-structures/8-1-singly-linked-lists.js
@@ -0,0 +1,54 @@
+/**
+ * Chapter 8.1 - Singly Linked Lists
+ * 
+ * Contains a head, tail, and length property.
+ * Consists of nodes, where each node has a value and pointer to another node/null.
+ * Think of it as a train, where cars are connected to another.
+ * No random access; you may not move straight to n=5, you have to iterate through all previous nodes.
+ * There are no indexes. If you insert into the middle of an array, every value must be re-indexed,
+ * which is why SLL's are better for insertion/deletion.
+ * 
+ * Example
+ * [4]->[6]->[8]->[2]->null
+ * Head = 4
+ * Tail = 2
+ * Length = 4
+ * 
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// naive way to add nodes to the end
+// let first = new Node('hi')
+// first.next = new Node('there')
+// first.next.next = new Node('how')
+// first.next.next.next = new Node('are')
+// first.next.next.next.next = new Node ('you')
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val)
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length ++;
+    return this;
+  }
+}
