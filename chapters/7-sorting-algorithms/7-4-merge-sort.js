3d669b14641d4e335119b04980dedba93c353967
mtthdz <matt@mtthdz.com>
2024-09-10 18:11:04 -0400

    merge sort function

--- chapters/7-sorting-algorithms/7-4-merge-sort.js
+++ chapters/7-sorting-algorithms/7-4-merge-sort.js
@@ -73,4 +73,22 @@ function merge(arr1, arr2) {
   return results;
 }
 
-merge([1, 10, 50], [2, 14, 99, 100]);
+/**
+ * MergeSort Pseudocode
+ * 
+ * 1. break up array into halves into single indices via .slice
+ * 2. merge arrays back
+ */
+function mergeSort(arr) {
+  // base case
+  if (arr.length <= 1) return arr;
+
+  // recursive case
+  let mid = Math.floor(arr.length/2)
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+mergeSort([10, 24, 76, 73, 72, 1, 9])
