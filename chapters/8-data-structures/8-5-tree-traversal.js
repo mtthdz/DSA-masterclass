a628266f93c6229db26e173609a6dd6d07b4c238
mtthdz <matt@mtthdz.com>
2024-09-26 15:52:12 -0400

    depth first search, preorder implementation

--- chapters/8-data-structures/8-5-tree-traversal.js
+++ chapters/8-data-structures/8-5-tree-traversal.js
@@ -3,24 +3,6 @@
  * 
  * Two ways to traverse a tree, (1) breadth first, and (2) depth first.
  * In essence BFS is horizontal, DFS is vertical.
- * 
- * Breadth first search
- * We want to visit every sibling node in a row before traversing down
- * the tree.
- * We typically use a queue to track progression
- * Starting at the root, we check for left then right. Each visited node will
- * be tracked via queue, and then later moved into the returned arr
- * 
- * Illustration:
- *    10
- *  6    15
- * 3 8     20
- * 
- * 1 - queue [10]        visited []
- * 2 - queue [6, 15]     visited [10]
- * 3 - queue [15]        visited [10, 6]
- * 4 - queue [15, 3, 8]  visited [10, 6]
- * 5 - queue [3, 8, 20]  visited [10, 6, 15]
  */
 class Node {
   constructor(value) {
@@ -35,6 +17,25 @@ class BST {
     this.root = null;
   }
 
+  /**
+   * Breadth first search
+   * We want to visit every sibling node in a row before traversing down
+   * the tree.
+   * We typically use a queue to track progression
+   * Starting at the root, we check for left then right. Each visited node will
+   * be tracked via queue, and then later moved into the returned arr
+   * 
+   * Illustration:
+   *    10
+   *  6    15
+   * 3 8     20
+   * 
+   * 1 - queue [10]        visited []
+   * 2 - queue [6, 15]     visited [10]
+   * 3 - queue [15]        visited [10, 6]
+   * 4 - queue [15, 3, 8]  visited [10, 6]
+   * 5 - queue [3, 8, 20]  visited [10, 6, 15]
+   */
   BFS() {
     let data  = [];
     let queue = [];
@@ -48,6 +49,32 @@ class BST {
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
     }
+    return data;
+  }
+
+  /**
+   * Depth first search, preorder
+   * We want to visit every child node below to the left first, and then check right
+   * We'll recursively check left to right for every node
+   * 
+   * illustration:
+   *     10
+   *  6    15
+   * 3 8     20
+   * 
+   * returns: [10, 6, 3, 8, 15, 20]
+   */
+  DFSPreOrder() {
+    let data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      data.push(node);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(current);
     return data;
   }
 }
